11b9b086cb950d94934d7cc12b4474cc6b03dabb
FFAI888 <453870150@qq.com>
2025-09-25 17:35:52 +0800

    Update Whitelist.sol

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 /**
  * @title Whitelist 合约
- * @dev 提供白名单管理功能，支持添加/删除白名单，查询用户是否在白名单，支持所有权转移。
+ * @dev 提供白名单管理功能：添加/删除白名单、查询白名单、管理员转移。
+ * 部署者（msg.sender）在构造函数中成为初始管理员（owner）。
  */
 contract Whitelist {
     address private _owner;
@@ -14,7 +15,7 @@ contract Whitelist {
     event RemovedFromWhitelist(address indexed account);
 
     constructor() {
-        _owner = msg.sender; // 部署者地址会成为管理员
+        _owner = msg.sender;                // 部署者即管理员
         emit OwnershipTransferred(address(0), _owner);
     }
 
@@ -23,12 +24,12 @@ contract Whitelist {
         _;
     }
 
+    /// @notice 当前管理员地址
     function owner() public view returns (address) {
         return _owner;
     }
 
     /// @notice 转移管理员权限
-    /// @param newOwner 新的管理员地址
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner is zero address");
         require(newOwner != _owner, "New owner is the same as current owner");
@@ -36,25 +37,22 @@ contract Whitelist {
         _owner = newOwner;
     }
 
-    /// @notice 添加白名单
-    /// @param account 用户地址
+    /// @notice 查询是否在白名单（登录页使用）
+    function isWhitelisted(address account) public view returns (bool) {
+        return _whitelist[account];
+    }
+
+    /// @notice 添加白名单（管理员）
     function addWhitelist(address account) public onlyOwner {
         require(!_whitelist[account], "Already whitelisted");
         _whitelist[account] = true;
         emit AddedToWhitelist(account);
     }
 
-    /// @notice 移除白名单
-    /// @param account 用户地址
+    /// @notice 移除白名单（管理员）
     function removeWhitelist(address account) public onlyOwner {
         require(_whitelist[account], "Not in whitelist");
         _whitelist[account] = false;
         emit RemovedFromWhitelist(account);
-    }
-
-    /// @notice 查询是否在白名单
-    /// @param account 用户地址
-    function isWhitelisted(address account) public view returns (bool) {
-        return _whitelist[account];
     }
 }
