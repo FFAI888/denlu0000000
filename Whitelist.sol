fe3337a4b6920b2fe424bf65a7ef29c4805bb8cf
FFAI888 <453870150@qq.com>
2025-09-25 15:52:36 +0800

    Update Whitelist.sol

--- Whitelist.sol
+++ Whitelist.sol
@@ -14,7 +14,7 @@ contract Whitelist {
     event RemovedFromWhitelist(address indexed account);
 
     constructor() {
-        _owner = msg.sender;
+        _owner = msg.sender; // 部署者地址会成为管理员
         emit OwnershipTransferred(address(0), _owner);
     }
 
@@ -27,24 +27,33 @@ contract Whitelist {
         return _owner;
     }
 
+    /// @notice 转移管理员权限
+    /// @param newOwner 新的管理员地址
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner is zero address");
+        require(newOwner != _owner, "New owner is the same as current owner");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
 
+    /// @notice 添加白名单
+    /// @param account 用户地址
     function addWhitelist(address account) public onlyOwner {
         require(!_whitelist[account], "Already whitelisted");
         _whitelist[account] = true;
         emit AddedToWhitelist(account);
     }
 
+    /// @notice 移除白名单
+    /// @param account 用户地址
     function removeWhitelist(address account) public onlyOwner {
         require(_whitelist[account], "Not in whitelist");
         _whitelist[account] = false;
         emit RemovedFromWhitelist(account);
     }
 
+    /// @notice 查询是否在白名单
+    /// @param account 用户地址
     function isWhitelisted(address account) public view returns (bool) {
         return _whitelist[account];
     }
