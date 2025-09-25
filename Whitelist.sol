ef686a281735c5aafe059bec371975cbbb31ec60
FFAI888 <453870150@qq.com>
2025-09-25 12:24:47 +0800

    Update Whitelist.sol

--- Whitelist.sol
+++ Whitelist.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 contract Whitelist {
     address public owner;
     mapping(address => bool) private whitelist;
+    address[] private whitelistArray; // ✅ 保存所有曾经加入过的地址
 
     event Added(address indexed user);
     event Removed(address indexed user);
@@ -25,16 +26,29 @@ contract Whitelist {
     }
 
     function addWhitelist(address user) external onlyOwner {
+        require(!whitelist[user], "Already whitelisted");
         whitelist[user] = true;
+        whitelistArray.push(user); // ✅ 保存进数组
         emit Added(user);
     }
 
     function removeWhitelist(address user) external onlyOwner {
+        require(whitelist[user], "Not whitelisted");
         whitelist[user] = false;
         emit Removed(user);
     }
 
     function isWhitelisted(address user) external view returns (bool) {
         return whitelist[user];
+    }
+
+    // ✅ 新增：返回所有曾经添加过的地址 + 当前状态
+    function getAllWhitelist() external view returns (address[] memory, bool[] memory) {
+        uint256 len = whitelistArray.length;
+        bool[] memory statuses = new bool[](len);
+        for (uint256 i = 0; i < len; i++) {
+            statuses[i] = whitelist[whitelistArray[i]];
+        }
+        return (whitelistArray, statuses);
     }
 }
