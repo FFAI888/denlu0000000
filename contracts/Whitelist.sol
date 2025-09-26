184b32fed89a2e1a1ae669792cbd65ddd4f5399c
FFAI888 <453870150@qq.com>
2025-09-26 16:18:43 +0800

    Update Whitelist.sol

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -3,52 +3,37 @@ pragma solidity ^0.8.0;
 
 contract Whitelist {
     address public owner;
-    mapping(address => bool) public whitelisted;
-    address[] private whitelistArr;
+    mapping(address => bool) public whitelist;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event WhitelistUpdated(address indexed user, bool status);
-
-    constructor() {
-        owner = msg.sender;
-    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         _;
     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Invalid address");
+    constructor() {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Zero address");
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 
-    function setWhitelisted(address user, bool status) public onlyOwner {
-        whitelisted[user] = status;
+    function setWhitelisted(address user, bool status) external onlyOwner {
+        whitelist[user] = status;
         emit WhitelistUpdated(user, status);
-        bool exists = false;
-        for(uint i=0;i<whitelistArr.length;i++){
-            if(whitelistArr[i]==user){ exists=true; break; }
-        }
-        if(status && !exists){
-            whitelistArr.push(user);
-        }else if(!status && exists){
-            for(uint i=0;i<whitelistArr.length;i++){
-                if(whitelistArr[i]==user){
-                    whitelistArr[i] = whitelistArr[whitelistArr.length-1];
-                    whitelistArr.pop();
-                    break;
-                }
-            }
-        }
     }
 
-    function isWhitelisted(address user) public view returns(bool){
-        return whitelisted[user];
+    function isWhitelisted(address user) external view returns (bool) {
+        return whitelist[user];
     }
 
-    function getWhitelist() public view returns(address[] memory){
-        return whitelistArr;
+    function getWhitelist(address user) external view returns (bool) {
+        return whitelist[user];
     }
 }
