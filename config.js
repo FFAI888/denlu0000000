08bb634badb20247c29dab1f815241b5a1f457b4
FFAI888 <453870150@qq.com>
2025-09-27 08:55:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-// version: v1.02
-const APP_VERSION = "v1.02";
+// version: v1.04
+const APP_VERSION = "v1.04";
 
 // 仅支持 BSC 主网
 const SUPPORTED_CHAIN_HEX = "0x38";
 const SUPPORTED_CHAIN_DEC = 56;
 
+// 本地存储键
 const LS_KEY = {
-  SESSION: "session", // {addr, chainId, ts}
+  SESSION: "session",     // {addr, chainId, ts}
+  THEME:   "themeColors", // {main, secondary}
 };
